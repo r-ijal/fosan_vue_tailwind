3bb86bcb3d9b629d2c0c5bb14637d8767c6dd6a1
r-ijal <r3dlabelz@gmail.com>
2024-12-08 16:11:08 +0700

    hiring partner

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -7,8 +7,8 @@
   <AboutSection />
   <!-- What -->
   <Syllabus />
-
   <!-- Partner -->
+   <Partner />
   <!-- Testimonies -->
   <!-- Join Now -->
   <!-- Footer -->
@@ -19,5 +19,6 @@ import NavBar from "../components/Home/NavBar.vue";
 import HeroSection from "../components/Home/HeroSection.vue";
 import AboutSection from "../components/Home/AboutSection.vue";
 import Syllabus from "../components/Home/Syllabus.vue";
+import Partner from "../components/Home/Partner.vue";
 </script>
 <style scoped></style>
